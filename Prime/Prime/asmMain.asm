6b3a9e8a3f453f6e659a646338086503bad166f6
dogqqq <43163572+dogqqq@users.noreply.github.com>
2019-10-07 12:51:05 +0800

    Update asmMain.asm

--- Prime/Prime/asmMain.asm
+++ Prime/Prime/asmMain.asm
@@ -23,22 +23,22 @@ inputInt DWORD 0
 .code
 asmMain PROC C	
 	INVOKE scanf, ADDR inInt, ADDR inputInt
-	MOV ebx, 0
+	MOV ebx, 0		;innitialize counter
 While_start:
-	SUB inputInt, 1
-	CMP inputInt, 1
-	JE While_end
-	INVOKE isPrime, inputInt
-	CMP eax, 0
-	JE L1
-	CMP ebx, 10
-	JE NewLine
-	INVOKE printf, ADDR strInt, inputInt
-	ADD ebx, 1
+	SUB inputInt, 1	
+	CMP inputInt, 1	
+	JE While_end	;end while if check value equals to 1
+	INVOKE isPrime, inputInt	;check if it is a prime number
+	CMP eax, 0		;check the return value from C funtion
+	JE L1			;if it is not a prime number, jump to L1
+	CMP ebx, 10		;check if it had already output 10 numbers
+	JE NewLine		;change to new line when output 10 prime numbers
+	INVOKE printf, ADDR strInt, inputInt	;otherwise, just output a number with a space
+	ADD ebx, 1		;count how many number had been outputed
 	JMP L1
-NewLine:
-	INVOKE printf, ADDR strIntN, inputInt
-	SUB ebx, 10
+NewLine:	
+	INVOKE printf, ADDR strIntN, inputInt	;output a number with \n
+	SUB ebx, 10		;reduce counter to 0
 L1:
 	JMP While_start
 While_end:
